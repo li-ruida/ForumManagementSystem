b63745c253fc569e7d5b5225d8cba177c3801a58
lrd <2605209238@qq.com>
2022-08-16 16:06:26 +0800

    sql

--- src/sql/ourforum_users.sql
+++ src/sql/ourforum_users.sql
@@ -22,6 +22,9 @@
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
+
+
+
 CREATE TABLE `users` (
   `Accounts` varchar(16) NOT NULL DEFAULT '账号',
   `Passwords` varchar(16) NOT NULL DEFAULT '密码',
